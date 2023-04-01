9cd91834916f77dd08af5de416f8ac9eb47492ba
Ashmin <dbswoals0512@gmail.com>
2023-04-01 20:35:47 +0900

    프로젝트 초기화

---
+++ src/main/java/com/yakgwa/catchme/CatchmeApplication.java
@@ -0,0 +1,13 @@
+package com.yakgwa.catchme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yakgwa/catchme/CatchmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakgwa.catchme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
